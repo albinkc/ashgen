f7d2dde6b50dac8c401f3e33cbb62c658aff6c11
Albin Kocheril Chacko <8876252+albinkc@users.noreply.github.com>
2025-07-29 23:01:26 +0530

    add base for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ashgen/", // Replace with your actual repo name
   plugins: [svelte()],
-})
+});
